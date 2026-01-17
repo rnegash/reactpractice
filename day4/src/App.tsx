b13047ad03b950be380fd39c2d9ad555e7ba79de
rnegash <rnegash@gmail.com>
2026-01-17 13:38:53 +0100

    day4 partially working

--- day4/src/App.tsx
+++ day4/src/App.tsx
@@ -1,20 +1,83 @@
 import { useState } from "react";
-
 import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0);
+const MemoryGame = ({ images }: { images: string[] }) => {
+  const game = [...images, ...images];
+  const [currentSelected, setCurrentSelected] = useState<number | null>();
+  const [solved, setSolved] = useState<string[]>([]);
+  const [currentGuesses, setCurrentGuesses] = useState<
+    [[number, string], [number, string]?] | null
+  >();
 
+  if (Array.isArray(currentGuesses) && currentGuesses.length === 2) {
+    if (currentGuesses[1] && currentGuesses[0][1] === currentGuesses[1][1]) {
+      setSolved((solved) => [...solved, currentGuesses[0][1]]);
+      console.log(solved);
+      setCurrentGuesses(null);
+    } else {
+      setCurrentGuesses(null);
+    }
+  }
+
+  const cardState = (image: string, idx: number) => {
+    const isSolved = solved.findIndex((solved) => solved === image) !== -1;
+
+    if (!currentGuesses) return "brightness(0.3)";
+
+    const isSelected =
+      currentGuesses?.findIndex((guess) => guess && guess[0] === idx) > -1;
+
+    if (isSolved) return "grayscale(100%)";
+
+    if (isSelected) return "initial";
+
+    return "brightness(0.3)";
+  };
+
+  return (
+    <div className="game">
+      {game.map((image, idx) => (
+        <img
+          key={idx}
+          src={image}
+          style={{
+            filter: cardState(image, idx),
+          }}
+          onClick={() => {
+            if (
+              currentSelected === idx ||
+              solved.findIndex((solved) => solved === image) !== -1
+            ) {
+              return;
+            }
+            setCurrentSelected(idx);
+            setCurrentGuesses((currentGuesses) => {
+              return currentGuesses
+                ? [currentGuesses[0], [idx, image]]
+                : [[idx, image]];
+            });
+          }}
+        ></img>
+      ))}
+    </div>
+  );
+};
+
+function App() {
   return (
     <>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <MemoryGame
+          images={[
+            "https://images.unsplash.com/photo-1626808642875-0aa545482dfb",
+            "https://images.unsplash.com/photo-1546842931-886c185b4c8c",
+            "https://images.unsplash.com/photo-1520763185298-1b434c919102",
+            "https://images.unsplash.com/photo-1442458017215-285b83f65851",
+            "https://images.unsplash.com/photo-1496483648148-47c686dc86a8",
+            "https://images.unsplash.com/photo-1591181520189-abcb0735c65d",
+          ]}
+        />
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
